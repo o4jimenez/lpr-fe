6d9f891cb8d231ec9c04e58a1100841041f5cec7
Oscar Jimenez <o4jimene@ucsd.edu>
2022-09-27 21:21:15 -0700

    react route path change

--- src/App.js
+++ src/App.js
@@ -15,9 +15,15 @@ function App() {
       <Navigator />
 
       <Routes>
-        <Route path={"/rentals"} element={<Rentals />} />
-        <Route path={"/contact_us"} element={<Contact />} />
-        <Route path={"/"} element={<Home />} />
+        <Route
+          path={process.env.PUBLIC_URL + "/rentals"}
+          element={<Rentals />}
+        />
+        <Route
+          path={process.env.PUBLIC_URL + "/contact_us"}
+          element={<Contact />}
+        />
+        <Route path={process.env.PUBLIC_URL + "/"} element={<Home />} />
       </Routes>
       <Footer />
     </Router>
